31175baa829b4dc7010d2d852929924bf5298b37
Johannes <yohans_yohans@yahoo.com>
2025-10-27 18:35:47 +0100

    hi

--- innlevering2/db-tilkobling.php
+++ innlevering2/db-tilkobling.php
@@ -4,10 +4,10 @@
    Kobler PHP til MySQL-databasen på Dokploy
 */
 
-$host = "mysql";                 // DB_HOST fra Dokploy
+$host = "b-studentsql-1.usn.no";                 // DB_HOST fra Dokploy
 $username = "255435";              // DB_USER fra Dokploy
 $password = "7e60255435";             // DB_PASSWORD fra Dokploy
-$database = "db_255435";  // DB_DATABASE fra Dokploy
+$database = "255435";  // DB_DATABASE fra Dokploy
 
 $db = mysqli_connect($host, $username, $password, $database)
   or die("Feil: ikke kontakt med database-server");
